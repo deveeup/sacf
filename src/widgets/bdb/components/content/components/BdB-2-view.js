57c64ec88530f6ad211014dcaa37d92c090bd698
David Rojas <jossedaviid1@gmail.com>
2018-03-23 13:00:32 -0500

    two content ok

--- src/widgets/bdb/components/content/components/BdB-2-view.js
+++ src/widgets/bdb/components/content/components/BdB-2-view.js
@@ -3,13 +3,43 @@ import React from 'react'
 import './BdB-2.styl'
 const Parser = require('html-react-parser')
 
-
-function BdB1View(props){
+function BdB2View(props){
+  const text = props.TextContent.texts
   return(
-    <div className="BdB-1">
-      {props.TextContent.title}
+    <div className="BdB-2">
+      <h2>{props.TextContent.title}</h2>
+      <div className="BdB-2-Content">
+        <div className="Left">
+          <figure>
+            <img src={props.LogoConsolidate} />
+          </figure>
+          <figure>
+            <img src={props.LogoInfo} />
+          </figure>
+          <figure>
+            <img src={props.LogoProcess} />
+          </figure>
+          <figure>
+            <img src={props.LogoProtection} />
+          </figure>
+          <figure>
+            <img src={props.LogoWarranty} />
+          </figure>
+        </div>
+        <div className="Right">
+          {
+            text.map((item) => {
+              return(
+                <p>
+                  {item.text}
+                </p>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   )
 }
 
-export default BdB1View
+export default BdB2View
