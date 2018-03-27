112470d0dc3f0106e4842c81add8c10c2fc73b06
David Rojas <jossedaviid1@gmail.com>
2018-03-27 17:52:40 -0500

    modal BdB #4 ok

--- src/widgets/bdb/components/content/components/BdB-4-view.js
+++ src/widgets/bdb/components/content/components/BdB-4-view.js
@@ -4,41 +4,23 @@ import './BdB-4.styl'
 const Parser = require('html-react-parser')
 
 function BdB4View(props){
-  const Items = props.TextContent.items
+  const item = props.Info.item
   return(
     <div className="BdB-4">
-      <h2>{props.TextContent.title}</h2>
+      <h2 className="Title">{props.Info.title}</h2>
       <div className="InfoContainer">
-        <div className="ImageContainer">
-          <figure>
-            <img src={props.LogoPolitics} />
-          </figure>
-          <figure>
-            <img src={props.LogoMethods} />
-          </figure>
-          <figure>
-            <img src={props.LogoDocument} />
-          </figure>
-          <figure>
-            <img src={props.LogoStructure} />
-          </figure>
-          <figure>
-            <img src={props.LogoTraining} />
-          </figure>
-          <figure>
-            <img src={props.LogoSaveMoney} />
-          </figure>
-          <figure>
-            <img src={props.LogoCash} />
-          </figure>
-        </div>
         <div className="ItemsContainer">
           {
-            Items.map((item) => {
+            item.map((item) => {
               return(
                 <div className="ItemContainer" key={item.id}>
-                  <h3>{item.title}</h3>
-                  <p>{item.text}</p>
+                  <figure className="ImageContainer">
+                    <img src={item.image}/>
+                  </figure>
+                  <div className="Info">
+                    <h3>{item.title}</h3>
+                    <p>{item.text}</p>
+                  </div>
                 </div>
               )
             })
--- src/widgets/bdb/components/content/containers/BdB-4.js
+++ src/widgets/bdb/components/content/containers/BdB-4.js
@@ -3,26 +3,12 @@ import React, { Component } from 'react'
 import BdB4View from '../components/BdB-4-view'
 import TextContent from './assets/BdB-4-Texts.json'
 
-import LogoPolitics from './assets/logo-politics.png'
-import LogoMethods from './assets/logo-method.png'
-import LogoDocument from './assets/logo-document.png'
-import LogoStructure from './assets/logo-structure.png'
-import LogoTraining from './assets/logo-training.png'
-import LogoSaveMoney from './assets/logo-saveMoney.png'
-import LogoCash from './assets/logo-cash.png'
 
 class BdB4 extends Component {
   render(){
     return(
       <BdB4View
-        TextContent={TextContent}
-        LogoPolitics={LogoPolitics}
-        LogoMethods={LogoMethods}
-        LogoDocument={LogoDocument}
-        LogoStructure={LogoStructure}
-        LogoTraining={LogoTraining}
-        LogoSaveMoney={LogoSaveMoney}
-        LogoCash={LogoCash}
+        Info={this.props.Info}
       />
     )
   }
