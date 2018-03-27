6b2276473965af993773c7a54870efcac01717a4
David Rojas <jossedaviid1@gmail.com>
2018-03-27 18:19:09 -0500

    remove file fixed

--- src/widgets/bdb/components/content/containers/BdB-4.js
+++ src/widgets/bdb/components/content/containers/BdB-4.js
@@ -1,8 +1,6 @@
 
 import React, { Component } from 'react'
 import BdB4View from '../components/BdB-4-view'
-import TextContent from './assets/BdB-4-Texts.json'
-
 
 class BdB4 extends Component {
   render(){
