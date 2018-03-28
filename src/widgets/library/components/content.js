f69254b97c945796e296b5428dd740fc673e541a
David Rojas <jossedaviid1@gmail.com>
2018-03-28 15:53:41 -0500

    Library section 1

--- src/widgets/library/components/content.js
+++ src/widgets/library/components/content.js
@@ -1,13 +1,13 @@
 
 import React from 'react'
 import Library1 from './content/containers/Library-1'
-// import Info from './content/containers/assets/info.json'
+import Info from './content/containers/assets/info.json'
 
 function ContentLibrary(props){
   switch(props.ContentLibrary){
     case 'Library-1':
       return(
-        <Library1/>
+        <Library1 Info={Info.A}/>
       )
     break
   }
--- src/widgets/library/components/content/components/Library-1-view.js
+++ src/widgets/library/components/content/components/Library-1-view.js
@@ -5,9 +5,37 @@ import './Library-1.styl'
 const Parse = require('html-react-parser')
 
 function LibraryView1 (props) {
+  const title = props.Info.title
+  const text = props.Info.text
+  const image = props.Info.image
+  const subtitle = props.Info.subtitle
+  const item = props.Info.item
   return(
     <div className="Library-1">
-      Library :)
+      <div className="Top">
+        <div className="Left">
+          <h2 className="Title">{Parse(title)}</h2>
+          <p className="Text">{text}</p>
+        </div>
+        <figure className="Right">
+          <img src={image} />
+        </figure>
+      </div>
+      <div className="Bottom">
+        <span className="Subtitle">{subtitle}</span>
+        <ol className="OrderList">
+          {
+            item.map((item) => {
+              return(
+                <li key={item.id}>
+                  <img src={item.icon} />  
+                  <p>{item.title}</p>
+                </li>
+              )
+            })
+          }
+        </ol>
+      </div>
     </div>
   )
 }
